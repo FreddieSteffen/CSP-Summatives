321e05ecb529444cad8f4c8b62c1b5713c331a66
FreddieSteffen <236309@shakopeeschools.org>
2025-11-13 11:26:47 -0600

    1.3.1
    began logic for if there is 4 in a row, along with moving the grey chip and dropping them

--- 1.2.5 - Shall We Play a Game/Freddie/a131_Childrens_Hospital.py
+++ 1.2.5 - Shall We Play a Game/Freddie/a131_Childrens_Hospital.py
@@ -9,10 +9,12 @@ num1 = 0
 num2 = 0
 
 #Lists
-EasyList = ["num1 + num2 = ", "num1 - num2 = "]
-MedList = []
-HardList = []
+Board = [(0,0,0,0,0,0,0),(0,0,0,0,0,0,0),(0,0,0,0,0,0,0),(0,0,0,0,0,0,0),(0,0,0,0,0,0,0),(0,0,0,0,0,0,0)]
+Rows = [0,1,2,3,4,5]
+Columns = [0,1,2,3,4,5,6]
 
+row1 = Rows[0]
+column1 = Columns[0]
 #Functions
 
 
@@ -31,6 +33,7 @@ wn.setup(width=1.0,height=1.0)
 wn.addshape(game_board)
 game_board_turtle = trtl.Turtle()
 game_board_turtle.shape(game_board)
+screen = trtl.Screen()
 
 #Buttons
 
@@ -47,17 +50,16 @@ YellowChip.shapesize(3.5)
 
 #Ask for player1/player2 name
 #Ask for what difficulty for both players
-def playernames():
-  playerName1 = trtl.textinput("Name", "What is player 1s name?")
-  while "," in playerName1 or len(playerName1) == 0 or "1" in playerName1 or "2" in playerName1 or "3" in playerName1 or "4" in playerName1 or "5" in playerName1 or "6" in playerName1 or "7" in playerName1 or "8" in playerName1 or "9" in playerName1:
-    playerName1 = trtl.textinput("Name", "Please do not use a comma, nothing, or number Enter your name")
-    break
+playerName1 = trtl.textinput("Name", "What is player 1s name?")
+while "," in playerName1 or len(playerName1) == 0 or "1" in playerName1 or "2" in playerName1 or "3" in playerName1 or "4" in playerName1 or "5" in playerName1 or "6" in playerName1 or "7" in playerName1 or "8" in playerName1 or "9" in playerName1:
+  playerName1 = trtl.textinput("Name", "Please do not use a comma, nothing, or number Enter your name")
+  break
 
-  playerName2 = trtl.textinput("Name", "What is player 2s name?")
-  while "," in playerName2 or len(playerName2) == 0 or "1" in playerName2 or "2" in playerName2 or "3" in playerName2 or "4" in playerName2 or "5" in playerName2 or "6" in playerName2 or "7" in playerName2 or "8" in playerName2 or "9" in playerName2:
-    playerName2 = trtl.textinput("Name", "Please do not use a comma, nothing, or number Enter your name")
-    break
-playernames()
+playerName2 = trtl.textinput("Name", "What is player 2s name?")
+while "," in playerName2 or len(playerName2) == 0 or "1" in playerName2 or "2" in playerName2 or "3" in playerName2 or "4" in playerName2 or "5" in playerName2 or "6" in playerName2 or "7" in playerName2 or "8" in playerName2 or "9" in playerName2:
+  playerName2 = trtl.textinput("Name", "Please do not use a comma, nothing, or number Enter your name")
+  break
+
 def PlayerDifficulty():
   playerDif = trtl.textinput("Difficulty", "What do you want the difficulty to be? Easy/Med/Hard")
   if playerDif == "Easy" or playerDif == "EASY" or playerDif == "easy":
@@ -177,21 +179,104 @@ def PlayerQuestionHard():
   randindex = rand.randint(1,9)
   index = index + randindex  
 
+GreyChip = trtl.Turtle()
+GreyChip.color("grey")
+GreyChip.shape('circle')
+GreyChip.shapesize(3.5)
+
+ychip = 0
+y = ychip
+
+x = 0
+
 #----Grey Chip Hover
+def OnClick(x, y):
+  #640x480
+  #Column 1
+  if -320 < x < -230 and -240 < y < 240:
+    GreyChip.penup()
+    x = -270
+    GreyChip.goto(x, ychip)
+    #FINISH TOMMOROW
+    for i in range():
+      Board[0][0] = 1
+  #Column 2
+  elif -230 < x < -140 and -240 < y < 240: 
+    GreyChip.penup()
+    x = -180
+    GreyChip.goto(x, ychip)
+  #Column 3
+  elif -140 < x < -50 and -240 < y < 240:
+    GreyChip.penup()
+    x = -90
+    GreyChip.goto(x, ychip)
+  #Column 4
+  elif -50 < x <40 and -240 < y < 240:
+    GreyChip.penup()
+    x = 0
+    GreyChip.goto(x, ychip)
+  #Column 5
+  elif 40 < x < 130 and -240 < y < 240:
+    GreyChip.penup()
+    x = 90
+    GreyChip.goto(x, ychip)
+  #Column 6
+  elif 130 < x < 220 and -240 < y < 240:
+    GreyChip.penup()
+    x = 180
+    GreyChip.goto(x, ychip)
+  #Column 7
+  elif 220 < x < 320 and -240 < y < 240:
+    GreyChip.penup()
+    x = 270
+    GreyChip.goto(x, ychip)
+
+ChipBelow = False
 
 #Chips drop
+CurrentX = GreyChip.xcor()
+CurrentY = 270
+def ChipsDrop(x, y):
+  global CurrentX, CurrentY
+  if -240 < x < 240 and ChipBelow == False:
+    if playermove % 2 == 0:
+      RedChip.penup()
+      RedChip.goto(CurrentX,CurrentY)
+      RedChip.setheading(90)
+      while GreyChip.ycor() >= -240:
+        RedChip.back(80)
+      print(CurrentY)
+
+    else:
+      YellowChip.penup()
+      YellowChip.goto(CurrentX,CurrentY)
+      YellowChip.setheading(90)
+      while GreyChip.ycor() >= -240:
+        YellowChip.back(80)
+      print(CurrentY)
+
 
 #Game logic for if someone wins
+def GameLogic():
+  print()
+  #if (row1,column1) :
+
 
 #Print Winner
 
+
 #Ask for replay
 def playagain():
-  playernames()
-  PlayerDifficulty()
+  replayGame = trtl.textinput("Replay?", "Do you want to play the game again? Yes or No")
+  if replayGame == "yes" or play_game == "YES" or play_game == "Yes" or play_game == "y" or play_game =="Y":
+    print("Working")
+    PlayerDifficulty()
+  else: 
+    print("Restart if you want to play")  
 
 #Play Game
-playagain()
+screen.onclick(OnClick)
+GreyChip.onclick(ChipsDrop)
 
 #Keeping whats on the screen there
 wn = trtl.Screen()
